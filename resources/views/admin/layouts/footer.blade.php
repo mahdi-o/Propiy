eff905a93a6694658fba07bae62f8b88dc21f753
mahdi-o <momohammadmahdi@gmail.com>
2025-04-02 18:27:41 +0330

    finish design row 3 in page User Account

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -3,7 +3,7 @@
       <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
       
         <div class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none ">
-          <div class="text-sm mr-50 leading-normal text-center text-slate-500 lg:text-start">  
+          <div class="text-sm mr-25 leading-normal text-center text-slate-500 lg:text-start">  
             <a href="#" class="font-semibold text-slate-700 dark:text-white" target="_blank">Propiy</a>
           </div>
         </div>
--- resources/views/admin/pages/user-info.blade.php
+++ resources/views/admin/pages/user-info.blade.php
@@ -1,44 +1,16 @@
-@extends('admin.layouts.master')
+@extEnds('admin.layouts.master')
 @section('content')
-    {{-- <div class="w-full px-5 py-6 mx-auto">
-        <!-- cards row 1 -->
-        <div class="w-full max-w-full px-3 mb-6 ">
-            <div
-                class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
-                <div class="flex-auto p-4">
-                    <div class="flex flex-row">
-                        <div
-                            class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
-                            <i class="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"></i>
-                        </div>
-                        <div class="flex-wrap w-2/3 max-w-full px-3">
-                            <div>
-                                <p
-                                    class="mb-0 font-sans text-sm font-semibold leading-normal uppercase dark:text-white dark:opacity-60">
-                                    Today's Money</p>
-                                <h5 class="mb-2 font-bold dark:text-white">$53,000</h5>
-                                <p class="mb-0 dark:text-white dark:opacity-60">
-                                    <span class="text-sm font-bold leading-normal text-emerald-500">+55%</span>
-                                    since yesterday
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
---}}
+
 
     <div class="bg-blue-500 mx-auto py-20 px-5 rounded-2xl w-full">
 
-        <!-- start row 1 -->
+        <!-- Start Row 1 -->
         <div class="bg-white mx-auto w-full px-3 rounded-2xl py-4 my-4">
 
-            <!--start div icon user and Username user -->
+            <!--Start div icon user and Username user -->
             <div class="flex flex-row gap-2 items-center text-center">
 
-                <!--start div icon user  -->
+                <!--Start div icon user  -->
                 <div class="bg-gray-400 h-15 w-15 rounded-2xl text-center py-3">
 
                     <svg class="h-8 w-8 text-white fill-white" viewBox="0 0 24 24" stroke="currentColor">
@@ -47,19 +19,19 @@
                     </svg>
 
                 </div>
-                <!--end div icon user -->
+                <!--End div icon user -->
 
-                <!--start div text username and "username" -->
+                <!--Start div text username and "username" -->
                 <div class="flex flex-col items-center text-center justify-center pt-2">
                     <p class="mb-0 text-sm font-bold">Username</p>
                     <p class="mb-2 text-sm">Moahmmad</p>
                 </div>
-                <!--end div text username and "username" -->
+                <!--End div text username and "username" -->
 
             </div>
-            <!--end div icon user and Username user -->
+            <!--End div icon user and Username user -->
 
-            <!-- start info account user -->
+            <!-- Start info account user -->
             <div class="flex flex-row gap-2 items-center mt-4">
 
                 <div class="text-green-600 flex flex-row gap-2">
@@ -88,18 +60,22 @@
                 </div>
 
             </div>
-            <!-- end info account user -->
+            <!-- End info account user -->
 
         </div>
-        <!-- End row 1 -->
+        <!-- End Row 1 -->
+
+        <hr
+            class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
 
 
-        <!-- start row 2 -->
+        <!-- Start Row 2 -->
         <div class="flex flex-col gap-1">
 
             <p class="text-md text-black px-5 mb-2 font-bold">The story of your association with Propiy</p>
-            <p class="text-black text-sm px-7">We have illustrated the journey from the start of your path with PROPAY to this moment, so you can have a clearer understanding of the processes that have been carried out.</p>
-            <!-- start flex-row 1 in row 2 -->
+            <p class="text-black text-sm px-7">We have illustrated the journey from the Start of your path with PROPAY to
+                this moment, so you can have a clearer understanding of the processes that have been carried out.</p>
+            <!-- Start flex-row 1 in row 2 -->
             <div class="flex flex-col lg:flex-row gap-1 bg-white mx-auto w-full rounded-2xl">
 
                 <!--Start cards row 1 -->
@@ -220,9 +196,9 @@
                 <!--End cards row 5 -->
 
             </div>
-            <!-- end flex-row 1 in row 2 -->
+            <!-- End flex-row 1 in row 2 -->
 
-            <!-- start flex-row 2 in row 2 -->
+            <!-- Start flex-row 2 in row 2 -->
             <div class="flex flex-col lg:flex-row gap-1 bg-white mx-auto w-full rounded-2xl">
 
                 <!--Start cards row 1 -->
@@ -343,8 +319,63 @@
                 <!--End cards row 5 -->
 
             </div>
-            <!-- end flex-row 2 in row 2 -->
+            <!-- End flex-row 2 in row 2 -->
 
         </div>
-        <!-- End row 2 -->
-    @endsection
+        <!-- End Row 2 -->
+
+
+        <hr class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
+
+        <!-- Start Row 3 -->
+        <div class="gb-white w-full py-4">
+            <p class="leading-normal uppercase dark:text-black dark:opacity-60 text-sm">Address</p>
+            <div class="flex flex-wrap -mx-3 ">
+
+                <div class="w-full max-w-full px-3 shrink-0 md:w-4/12 md:flex-0">
+                    <div class="mb-4">
+                        <label for="city"
+                            class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Province</label>
+                        <input type="text" name="city" value="New York"
+                            class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                </div>
+                <div class="w-full max-w-full px-3 shrink-0 md:w-4/12 md:flex-0">
+                    <div class="mb-4">
+                        <label for="country"
+                            class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">City</label>
+                        <input type="text" name="country" value="United States"
+                            class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                </div>
+                <div class="w-full max-w-full px-3 shrink-0 md:w-4/12 md:flex-0">
+                    <div class="mb-4">
+                        <label for="postal code"
+                            class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Postal
+                            code</label>
+                        <input type="text" name="postal code" value="437300"
+                            class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                </div>
+                <div class="w-full max-w-full px-3 shrink-0 md:w-full md:flex-0">
+                    <div class="mb-4">
+                        <label for="address"
+                            class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Address</label>
+                        <input type="text" name="address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                            class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                </div>
+
+                <div class="w-full max-w-full px-3 shrink-0 md:w-full md:flex-0 items-center justify-center text-right">
+                    <button type="button"
+                        class="inline-block px-8 py-2 mb-4 ml-auto font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-rose-500 border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Settings</button>
+                </div>
+
+            </div>
+            <hr
+                class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
+        </div>
+        <!-- End Row 3 -->
+
+    </div>
+    @Endsection
