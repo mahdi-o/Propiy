57ec0726f7060c0a3ec5f407b88c22ab96587d44
mahdi-o <momohammadmahdi@gmail.com>
2025-04-02 18:39:05 +0330

    debug  design row 3 in page User Account

--- resources/views/admin/pages/user-info.blade.php
+++ resources/views/admin/pages/user-info.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 
-    <div class="bg-blue-500 mx-auto py-20 px-5 rounded-2xl w-full">
+    <div class="bg-transparent mx-auto py-5 px-5 rounded-2xl w-full">
 
         <!-- Start Row 1 -->
         <div class="bg-white mx-auto w-full px-3 rounded-2xl py-4 my-4">
@@ -70,7 +70,8 @@
 
 
         <!-- Start Row 2 -->
-        <div class="flex flex-col gap-1">
+        <div class="bg-white mx-auto w-full px-3 rounded-2xl py-4 my-4">
+              <div class="flex flex-col gap-1">
 
             <p class="text-md text-black px-5 mb-2 font-bold">The story of your association with Propiy</p>
             <p class="text-black text-sm px-7">We have illustrated the journey from the Start of your path with PROPAY to
@@ -322,13 +323,16 @@
             <!-- End flex-row 2 in row 2 -->
 
         </div>
+        </div>
+      
         <!-- End Row 2 -->
 
 
         <hr class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
 
         <!-- Start Row 3 -->
-        <div class="gb-white w-full py-4">
+        <div class="bg-white mx-auto w-full px-3 rounded-2xl py-4 my-4">
+            <div class="gb-white w-full py-2">
             <p class="leading-normal uppercase dark:text-black dark:opacity-60 text-sm">Address</p>
             <div class="flex flex-wrap -mx-3 ">
 
@@ -366,15 +370,17 @@
                     </div>
                 </div>
 
-                <div class="w-full max-w-full px-3 shrink-0 md:w-full md:flex-0 items-center justify-center text-right">
+                <div class="w-full max-w-full px-3 shrink-0 md:w-full md:flex-0 items-center justify-center text-right mt-4">
                     <button type="button"
-                        class="inline-block px-8 py-2 mb-4 ml-auto font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-rose-500 border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Settings</button>
+                        class="inline-block px-8 py-2 mb-4 ml-auto font-bold leading-normal text-center text-gray-900 align-middle transition-all ease-in bg-rose-500 border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Settings</button>
                 </div>
 
             </div>
-            <hr
-                class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
+           
         </div>
+        </div>
+        <hr
+        class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
         <!-- End Row 3 -->
 
     </div>
